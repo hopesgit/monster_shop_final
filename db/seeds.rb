8e8808461722af7404143689ddf2a7a3d78f1e29
Hope Gochnour <hope.gochnour@gmail.com>
2020-11-10 11:06:27 -0700

    Add testing for bulk orders link on merchant show page for merchant_user

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,32 @@ brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'D
 megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+# Users
+user_1 = User.create!(name: "Melody",
+                      address: "222 Song Ln",
+                      city: "Harmony",
+                      state: "CO",
+                      zip: 80020,
+                      email: "melody@example.com",
+                      password: "Seekrit",
+                      role: 0,
+                      merchant_id: nil)
+user_2 = User.create!(name: "Alex",
+                      address: "1041 Circle Cir",
+                      city: "Denver",
+                      state: "CO",
+                      zip: 80012,
+                      email: "alex@example.com",
+                      password: "Trade Seekrit",
+                      role: 1,
+                      merchant_id: brian.id)
+user_3 = User.create!(name: "Hope",
+                      address: "222 Song Ln",
+                      city: "Harmony",
+                      state: "CO",
+                      zip: 80020,
+                      email: "hope@example.com",
+                      password: "Server Seekrit",
+                      role: 2,
+                      merchant_id: nil)
