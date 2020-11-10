c627fae34ab32da0fa60c0b9536ff57fbf329dc9
Hope Gochnour <hope.gochnour@gmail.com>
2020-11-10 14:17:59 -0700

    Add route, actions, and views for creating a new bulk discount

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -5,5 +5,26 @@ class Merchant::BulkDiscountsController < Merchant::BaseController
   end
 
   def new
+    @merchant = Merchant.find(current_user.merchant_id)
+    @discount = BulkDiscount.new
+  end
+
+  def create
+    discount = BulkDiscount.create!(
+      item_quantity: new_params[:item_quantity],
+      percentage: new_params[:percentage],
+      merchant_id: current_user.merchant_id)
+    if discount.save
+      flash[:success] = "Bulk discount created."
+      redirect_to merchant_merchant_bulk_discounts_path(discount.merchant_id)
+    else
+      flash.now[:warning] = "Form not filled out completely."
+      render :new
+    end
+  end
+
+  private
+  def new_params
+    params.require(:bulk_discount).permit(:item_quantity, :percentage)
   end
 end
